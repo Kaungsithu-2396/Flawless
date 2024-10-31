1c95fd52cfa0d0b523757bd887b62de4d2de5a62
Kaungsithu-123 <Kaungsithu2396@gmail.com>
2024-10-31 13:44:54 +0700

    revalidate

---
+++ src/app/api/webhook/route.ts
@@ -0,0 +1,20 @@
+import { revalidatePath } from "next/cache";
+import type { NextRequest } from "next/server";
+export async function POST(request: NextRequest) {
+    try {
+        const { paths } = await request.json();
+        console.log(paths);
+        for (const path of paths) {
+            revalidatePath(encodeURI(path));
+        }
+        return Response.json({
+            revalidated: true,
+            now: Date.now(),
+        });
+    } catch (error) {
+        return Response.json({
+            revalidated: false,
+            now: Date.now(),
+        });
+    }
+}
